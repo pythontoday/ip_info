9eadb2772942fd6ff401216489806398b8ae8b7a
Lanity <damned89098@gmail.com>
2022-03-09 04:13:09 +0300

    Map Marker
    Добавлен маркер по найденным координатам, а то непонятно где конкретно находится нужное место. Так же добавил простейшее рекурсивное меню, теперь можно запускать программу нативно через питон, или билдить в ехе, и данные не будут теряться.

--- main.py
+++ main.py
@@ -23,7 +23,11 @@ def get_info_by_ip(ip='127.0.0.1'):
         for k, v in data.items():
             print(f'{k} : {v}')
         
-        area = folium.Map(location=[response.get('lat'), response.get('lon')])
+        area = folium.Map(location=(response.get('lat'), response.get('lon')))
+
+        # Added Marker on location so it is lots easier to see the l
+        folium.Marker(location=(response.get('lat'), response.get('lon')), tooltip="Probably the location of the target.").add_to(area)
+
         area.save(f'{response.get("query")}_{response.get("city")}.html')
         
     except requests.exceptions.ConnectionError:
@@ -33,10 +37,19 @@ def get_info_by_ip(ip='127.0.0.1'):
 def main():
     preview_text = Figlet(font='slant')
     print(preview_text.renderText('IP INFO'))
-    ip = input('Please enter a target IP: ')
+
+    ip = input('Please enter a target IP or quit: ')
+    # simplest possible menu at 4 a.m.
     
+    if ip == 'quit': return
     get_info_by_ip(ip=ip)
+    main()
     
     
 if __name__ == '__main__':
-    main()
+    res = ''
+    while res.lower() not in ('quit', 'q'):
+        main()
+        while res not in ('quit', 'q', 'again', 'a'):
+            res = input('Enter quit/q or again/a: ')
+
